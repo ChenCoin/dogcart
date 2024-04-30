ea08191c60dc6264de4067a72636818307ad96c7
ChenCoin <chencoin.cn@gmail.com>
2024-05-01 00:05:21 +0800

    add meteor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ux.dart';
 import 'view/main_panel.dart';
+import 'view/meteor_board.dart';
 import 'web/conf_nil.dart' if (dart.library.html) 'web/conf_web.dart';
 
 void main() {
@@ -20,15 +21,19 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFFFAFAF8),
         useMaterial3: true,
       ),
-      home: Scaffold(backgroundColor: const Color(0xFFFAFAF8), body: home()),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFFAFAF8),
+        body: home(context),
+      ),
     );
   }
 
-  Widget home() {
+  Widget home(BuildContext context) {
     if (UX.debugMode) {
       return Stack(
         children: [
           const MyHomePage(),
+          MeteorBoard(size: MediaQuery.of(context).size),
           IgnorePointer(
             child: Container(
               decoration: const BoxDecoration(color: Color(0xD0000000)),
--- lib/view/meteor_board.dart
+++ lib/view/meteor_board.dart
@@ -1,3 +1,38 @@
-class MeteorBoard {
+import 'package:flutter/material.dart';
 
+class MeteorBoard extends StatefulWidget {
+  final Size size;
+
+  const MeteorBoard({super.key, required this.size});
+
+  @override
+  State<StatefulWidget> createState() => _MeteorState();
+}
+
+class _MeteorState extends State<MeteorBoard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: widget.size,
+      painter: _MyPainter(),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
+
+class _MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
