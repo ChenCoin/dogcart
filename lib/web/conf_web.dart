90ed300c3e599adf3da64864648a9ed20f51c207
ChenCoin <chencoin.cn@gmail.com>
2024-05-30 01:55:51 +0800

    add loading

--- lib/web/conf_web.dart
+++ lib/web/conf_web.dart
@@ -1,9 +1,11 @@
 import 'dart:html';
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 void webConfigure() {
   debugPrint('configure web');
+  js.context.callMethod('endLoading');
   setUrlStrategy(null);
   document.onContextMenu.listen((event) => event.preventDefault());
 }
